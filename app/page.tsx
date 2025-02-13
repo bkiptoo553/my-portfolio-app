22c548392c120c7c1e34fbd9d4df51f293c13194
bkiptoo553 <bkiptoo553@gmail.com>
2025-02-13 14:16:23 +0300

    portfolio-004

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
+import Hero from "@/components/hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>home page</h1>
-
+    <main className="relative bg-black-100 flex 
+    justify-center items-center flex-col
+    overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <Hero />
+      </div>
+    </main>
   );
 }
